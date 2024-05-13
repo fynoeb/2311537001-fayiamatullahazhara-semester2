245f6c88bcd57e5b37a0bac6fcc7bdb8701f0269
hp <hp@FTI-LAB03-PC11>
2024-05-13 11:05:42 +0700

    pekan 2

---
+++ StrukturData2024/src/pekan2/contohStack.java
@@ -0,0 +1,19 @@
+package pekan2;
+
+import java.util.Stack;
+
+public class contohStack {
+	public static void main(String[] args) {
+		ArrayStack test = new ArrayStack();
+		Integer[] a = {4, 8, 15, 16, 23, 42}; // autoboxing allows this
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("nilai A"+i+"= "+ a[i]);
+			test.push(a[i]);
+		}
+		System.out.println("size stacknya: "+test.size());
+		System.out.println("paling atas: "+ test.top());
+		System.out.println("nilainya "+test.pop());
+
+	}
+
+}
