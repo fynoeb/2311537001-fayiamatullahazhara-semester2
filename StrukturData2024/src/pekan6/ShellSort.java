a69eb440af840a3aef9196dc59018a89c8590789
hp <hp@FTI-LAB03-PC11>
2024-06-03 12:05:46 +0700

    pekan 6

---
+++ StrukturData2024/src/pekan6/ShellSort.java
@@ -0,0 +1,35 @@
+package pekan6;
+
+public class ShellSort {
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n ; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+	/* function to sort arr using shellsort */
+	int sort (int arr[]) {
+		int n = arr.length;
+		//start with a big gap, then reduce the gap
+		for (int gap = n/2;gap>0;gap/=2) {
+			for (int i=gap; i<n ; i +=1) {
+				int temp = arr[i];
+				int j;
+				for(j=i; j>= gap && arr[j-gap] > temp; j-=gap)
+					arr[j] = arr[j-gap];
+					arr[j]= temp;
+			}
+		}
+		return 0;
+	}
+	public static void main(String arg[]) {
+		int arr[] = {12, 34, 54, 2, 3,};
+		System.out.print("Data sebelum diurutkan: ");
+		printArray(arr);
+		ShellSort ob = new ShellSort();
+		ob.sort(arr);
+		System.out.print("Data Sesudah diurutkan menggunakan ShellSort: ");
+		printArray(arr);
+	}
+
+}
